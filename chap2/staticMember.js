7012f05e953674470e9f33ef64bc504a46f6a39d
OpenTech-Consult <opentech.consult@gmail.com>
2022-12-23 13:28:54 +0000

    HandsOnTypeScript
    static properties and methods

---
+++ chap2/staticMember.js
@@ -0,0 +1,11 @@
+var ClassA = /** @class */ (function () {
+    function ClassA() {
+    }
+    ClassA.getFullName = function () {
+        return "ClassA " + ClassA.typeName;
+    };
+    return ClassA;
+}());
+var a = new ClassA();
+console.log(ClassA.typeName);
+console.log(ClassA.getFullName());
---
+++ chap2/staticMember.ts
@@ -0,0 +1,13 @@
+class ClassA {
+    static typeName: string;
+
+    constructor() {}
+
+    static getFullName() {
+        return "ClassA " + ClassA.typeName;
+    }
+}
+
+const a = new ClassA();
+console.log(ClassA.typeName)
+console.log(ClassA.getFullName())
